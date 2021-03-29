0791e844a5dd724eaeb8803bd1c040e889696cef
Mathis <handymathis@gmail.com>
2021-03-29 10:07:06 +0200

    updated navbar design

--- components/navbar.php
+++ components/navbar.php
@@ -1,5 +1,12 @@
 <div class="nav-flex">
 	<nav>
+        <?php
 
+            $nav = wp_nav_menu(array(
+                'menu_class' => 'display-menu-ul'
+            ));
+            echo $nav;
+
+        ?>
 	</nav>
 </div>
